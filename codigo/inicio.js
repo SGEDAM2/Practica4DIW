0befc041bf03c45cd9674e0641e18a702b9346ee
Susana <susana_199621@hotmail.es>
2020-12-01 18:35:29 +0100

    imagenes de inicio, carrusel imagenes de inicio,..

--- codigo/inicio.js
+++ codigo/inicio.js
@@ -1,38 +1,48 @@
-let indice=0;
-function mostrarSeccion(){
-    let elemento=document.getElementsByTagName("section");
+/**
+ * Autor: SusanaGE
+ * Fecha:01-12-2020
+ */
+//VARIABLES
+//Variable que indica que sección se tiene que mostrar
+let indice = 0;
 
-    //Mostrar
-    if(indice == 0){
-        elemento[elemento.length-1].style.display="none";
-        elemento[indice].style.display="block";
-    }else{
-        elemento[indice-1].style.display="none";
-        elemento[indice].style.display="block";
-    }
+//FUNCIONES
+/**Función que realiza el funcionamiento de "carrusel" de las secciones.
+ * Dependiendo el indice muestra una sección u otra.
+ */
+function mostrarSeccion() {
+  let elemento = document.getElementsByTagName("section");
 
-    //Incrementar
-    if(indice == elemento.length - 1 )
-        indice = 0;
-    else
-        indice++;
-}
-window.setInterval(mostrarSeccion,5000);
-mostrarSeccion();
-//var slideIndex = 1;
-//showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  //Mostrar
+  if (indice == 0) {
+    elemento[elemento.length - 1].style.display = "none";
+    elemento[indice].style.display = "flex";
+  } else {
+    elemento[indice - 1].style.display = "none";
+    elemento[indice].style.display = "flex";
   }
-  x[slideIndex-1].style.display = "block";  
+
+  //Incrementar
+  if (indice == elemento.length - 1)
+    indice = 0;
+  else
+    indice++;
 }
+
+/**Función que oculta todas las secciones */
+function ocultarSecciones() {
+  let elementos = document.getElementsByTagName("section");
+  for (let i = 0; i < elementos.length; i++) {
+    elementos[i].style.display = "none";
+  }
+}
+
+//Ocultar todas las secciones
+ocultarSecciones();
+
+//Para que se ejecute la primera vez el carrusel
+mostrarSeccion();
+
+//Que las secciones cambien cada 5 segundos
+window.setInterval(mostrarSeccion, 3000);
+
