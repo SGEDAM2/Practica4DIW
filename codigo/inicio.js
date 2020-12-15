60dc1576187a7f9c98acdfce4cd95864eca30e5d
Susana <susana_199621@hotmail.es>
2020-12-15 13:53:33 +0100

    Inicio completo (revisar)

--- codigo/inicio.js
+++ codigo/inicio.js
@@ -5,6 +5,22 @@
 //VARIABLES
 //Variable que indica que sección se tiene que mostrar
 let indice = 0;
+let ancho ="50%";
+
+document.addEventListener("DOMContentLoaded", () => {
+  //Obtener el ancho de la sección segun el tamaño de la ventana
+  window.addEventListener("resize",cambiarAncho);
+  //Obtener el ancho la primera vez
+  cambiarAncho();
+
+  //CARRUSEL
+  //Ocultar todas las secciones
+  ocultarSecciones();
+  //Para que se ejecute la primera vez el carrusel
+  mostrarSeccion();
+  //Que las secciones cambien cada 5 segundos
+  window.setInterval(mostrarSeccion, 3000);
+})
 
 //FUNCIONES
 /**Función que realiza el funcionamiento de "carrusel" de las secciones.
@@ -37,12 +53,32 @@ function ocultarSecciones() {
   }
 }
 
-//Ocultar todas las secciones
-ocultarSecciones();
+/**Función que según el ancho de la ventana cambia el menú y las secciones */
+function cambiarAncho(){
+  let contenedor=document.getElementById("lista");
+  let cabecera=document.getElementsByTagName("header")[0];
+  let elementos=document.getElementsByClassName("opcion");
 
-//Para que se ejecute la primera vez el carrusel
-mostrarSeccion();
+  if(window.innerWidth < 576){
+    ancho="80%"; //Sección
 
-//Que las secciones cambien cada 5 segundos
-window.setInterval(mostrarSeccion, 3000);
+    //Menu
+    for(let i=0; i< elementos.length; i++)
+      elementos[i].style.display="none";
 
+    //contenedor.style.flexDirection="column";
+  }else{
+    ancho="50%" //Sección
+
+    //menu
+    contenedor.style.flexDirection="row";
+    cabecera.style.height="150px";
+    for(let i=0; i< elementos.length; i++)
+      elementos[i].style.display="block";
+  }
+  //Sección
+  elementos = document.getElementsByTagName("section");
+  for (let i = 0; i < elementos.length; i++) {
+    elementos[i].style.width = ancho;
+  }
+}
--- codigo/menu.js
+++ codigo/menu.js
@@ -1,15 +1,55 @@
 
 document.addEventListener("DOMContentLoaded",()=>{
+/*     //Obtener el ancho de la sección segun el tamaño de la ventana
+    window.addEventListener("resize",cambiarAncho);
+    //Obtener el ancho la primera vez
+    cambiarAncho(); */
     document.getElementById("menu").addEventListener("click",mostrarOcultar)
 })
 
 let mostrar=true;
 const mostrarOcultar = (e)=>{
-    console.log("aqui")
+    let contenedor=document.getElementById("lista");
+    let elementos=document.getElementsByClassName("opcion");
+    let cabecera=document.getElementsByTagName("header")[0];
     if(mostrar){
-        let elementos=document.getElementsByClassName("opcion");
+        contenedor.style.flexDirection="column";
+        cabecera.style.height="250px";
         for(let i=0; i< elementos.length; i++)
             elementos[i].style.display="block";
-        console.log("aqui")
+        /* cabecera.classList.add("menuMostrar");
+        cabecera.classList.remove("menuOcultar");
+        for(let i=0; i< elementos.length; i++){
+            elementos[i].classList.add("opcionMostrar");
+            elementos[i].classList.remove("opcionOcultar");
+        } */
+            
+        mostrar=false;
+    }else{
+        contenedor.style.flexDirection="row";
+        cabecera.style.height="150px";
+        for(let i=0; i< elementos.length; i++)
+            elementos[i].style.display="none";
+        /* cabecera.classList.add("menuOcultar");
+        cabecera.classList.remove("menuMostrar");
+        for(let i=0; i< elementos.length; i++){
+            elementos[i].classList.add("opcionOcultar");
+            elementos[i].classList.remove("opcionMostrar");
+        } */
+        mostrar=true; 
     }
 }
+
+/**Función que cambia las propiedades dependiendo del ancho de la ventana */
+/* function cambiarAncho(){
+    let contenedor=document.getElementById("lista");
+    let cabecera=document.getElementsByTagName("header")[0];
+    if(window.innerWidth < 576){
+        contenedor.style.flexDirection="column";
+    }
+    else{
+        contenedor.style.flexDirection="row";
+        cabecera.style.height="150px";
+        console.log("aqui");
+    }
+} */
