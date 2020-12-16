b68cb6134778302434e4722ef9ad193d69f41068
Susana <susana_199621@hotmail.es>
2020-12-16 19:21:51 +0100

    Inicio revisado

--- codigo/menu.js
+++ codigo/menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Funciones de la barra de navegación
  * 
@@ -30,52 +29,10 @@ const mostrarOcultar = (e) => {
         lista.style.flexDirection = "row";
         menu.style.backgroundColor="#6897DE";
         mostrar = true;
-=======
-
-document.addEventListener("DOMContentLoaded",()=>{
-/*     //Obtener el ancho de la sección segun el tamaño de la ventana
-    window.addEventListener("resize",cambiarAncho);
-    //Obtener el ancho la primera vez
-    cambiarAncho(); */
-    document.getElementById("menu").addEventListener("click",mostrarOcultar)
-})
-
-let mostrar=true;
-const mostrarOcultar = (e)=>{
-    let contenedor=document.getElementById("lista");
-    let elementos=document.getElementsByClassName("opcion");
-    let cabecera=document.getElementsByTagName("header")[0];
-    if(mostrar){
-        contenedor.style.flexDirection="column";
-        cabecera.style.height="250px";
-        for(let i=0; i< elementos.length; i++)
-            elementos[i].style.display="block";
-        /* cabecera.classList.add("menuMostrar");
-        cabecera.classList.remove("menuOcultar");
-        for(let i=0; i< elementos.length; i++){
-            elementos[i].classList.add("opcionMostrar");
-            elementos[i].classList.remove("opcionOcultar");
-        } */
-            
-        mostrar=false;
-    }else{
-        contenedor.style.flexDirection="row";
-        cabecera.style.height="150px";
-        for(let i=0; i< elementos.length; i++)
-            elementos[i].style.display="none";
-        /* cabecera.classList.add("menuOcultar");
-        cabecera.classList.remove("menuMostrar");
-        for(let i=0; i< elementos.length; i++){
-            elementos[i].classList.add("opcionOcultar");
-            elementos[i].classList.remove("opcionMostrar");
-        } */
-        mostrar=true; 
->>>>>>> 60dc1576187a7f9c98acdfce4cd95864eca30e5d
     }
 }
 
 /**Función que cambia las propiedades dependiendo del ancho de la ventana */
-<<<<<<< HEAD
 function cambiarAncho() {
     let nav = document.getElementById("navegacion");
     let lista = document.getElementById("lista");
@@ -86,17 +43,3 @@ function cambiarAncho() {
         lista.style.flexDirection = "row";
     }
 }
-=======
-/* function cambiarAncho(){
-    let contenedor=document.getElementById("lista");
-    let cabecera=document.getElementsByTagName("header")[0];
-    if(window.innerWidth < 576){
-        contenedor.style.flexDirection="column";
-    }
-    else{
-        contenedor.style.flexDirection="row";
-        cabecera.style.height="150px";
-        console.log("aqui");
-    }
-} */
->>>>>>> 60dc1576187a7f9c98acdfce4cd95864eca30e5d
